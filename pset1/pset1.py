8cf16f1040cde8a563f822b0dd723f50cc0c48f1
João Pedro Estevão Louback <125689387+joaolouback@users.noreply.github.com>
2024-03-27 16:41:26 -0300

    Add files via upload

---
+++ pset1/pset1.py
@@ -0,0 +1,400 @@
+# IDENTIFICAÇÃO DO ESTUDANTE:
+# Preencha seus dados e leia a declaração de honestidade abaixo. NÃO APAGUE
+# nenhuma linha deste comentário de seu código!
+#
+#    Nome completo: João Pedro Estevão Louback
+#    Matrícula: 202305625
+#    Turma: CC3M-A
+#    Email: jpestevaoloubackbr@gmail.com
+#
+# DECLARAÇÃO DE HONESTIDADE ACADÊMICA:
+# Eu afirmo que o código abaixo foi de minha autoria. Também afirmo que não
+# pratiquei nenhuma forma de "cola" ou "plágio" na elaboração do programa,
+# e que não violei nenhuma das normas de integridade acadêmica da disciplina.
+# Estou ciente de que todo código enviado será verificado automaticamente
+# contra plágio e que caso eu tenha praticado qualquer atividade proibida
+# conforme as normas da disciplina, estou sujeito à penalidades conforme
+# definidas pelo professor da disciplina e/ou instituição.
+
+
+# Imports permitidos (não utilize nenhum outro import!):
+import sys
+import math
+import base64
+import tkinter
+from io import BytesIO
+from PIL import Image as PILImage
+
+
+def caixa_desfoque(n):
+    """Cria um kernel de desfoque de caixa de tamanho n x n."""
+    valor = 1 / (n * n)
+    return [[valor] * n for _ in range(n)]
+
+# Classe Imagem:
+
+
+class Imagem:
+    def __init__(self, largura, altura, pixels):
+        self.largura = largura
+        self.altura = altura
+        self.pixels = pixels
+
+    def get_pixel(self, x, y):
+        if x < 0:
+            x = 0
+        elif x >= self.largura:
+            x = self.largura - 1
+        if y < 0:
+            y = 0
+        elif y >= self.altura:
+            y = self.altura - 1
+
+        return self.pixels[(x + y * self.largura)]
+
+    def set_pixel(self, x, y, c):
+        self.pixels[(x + y * self.largura)] = c
+
+    def aplicar_por_pixel(self, func):
+        resultado = Imagem.nova(self.largura, self.altura)
+        for x in range(resultado.largura):
+            for y in range(resultado.altura):
+                cor = self.get_pixel(x, y)
+                nova_cor = func(cor)
+                resultado.set_pixel(x, y, nova_cor)
+        return resultado
+
+    # Correlação kernel
+    def correlacao(self, kn):
+        k = len(kn)
+        centro = k // 2
+        final_img = Imagem.nova(self.largura, self.altura)
+
+        for x in range(final_img.largura):
+            for y in range(final_img.altura):
+                novacor = 0
+                for w in range(k):
+                    for h in range(k):
+                        x1 = x - centro + h
+                        y1 = y - centro + w
+                        novacor += self.get_pixel(x1, y1) * kn[w][h]
+
+                final_img.set_pixel(x, y, novacor)
+
+        return final_img
+
+    def invertida(self):
+        # Implementação da inversão dos pixels
+        return self.aplicar_por_pixel(lambda c: 255 - c)
+
+    def borrada(self, n):
+        kernel = caixa_desfoque(n)
+        return self.correlacao(kernel)
+
+    def focada(self, n):
+        # Criar o kernel de desfoque de caixa
+        kernel_desfoque = caixa_desfoque(n)
+
+        # Aplicar a correlação com o kernel de desfoque de caixa
+        imagem_desfocada = self.correlacao(kernel_desfoque)
+
+        # Criar a imagem nítida (máscara de não nitidez)
+        imagem_nitida = Imagem.nova(self.largura, self.altura)
+        for x in range(self.largura):
+            for y in range(self.altura):
+                valor_nitido = 2 * \
+                    self.get_pixel(x, y) - imagem_desfocada.get_pixel(x, y)
+                # Garantir que o pixel resultante esteja no intervalo [0, 255]
+                valor_nitido = max(0, min(valor_nitido, 255))
+                imagem_nitida.set_pixel(x, y, valor_nitido)
+
+        return imagem_nitida
+
+    def bordas(self):
+        # Aplicar o kernel Kx
+        kernel_Kx = [
+            [-1, 0, 1],
+            [-2, 0, 2],
+            [-1, 0, 1]
+        ]
+        imagem_Ox = self.correlacao(kernel_Kx)
+
+        # Aplicar o kernel Ky
+        kernel_Ky = [
+            [-1, -2, -1],
+            [0, 0, 0],
+            [1, 2, 1]
+        ]
+        imagem_Oy = self.correlacao(kernel_Ky)
+
+        # Calcular a imagem final com a raiz quadrada da soma dos quadrados de Ox e Oy
+        imagem_final = Imagem.nova(self.largura, self.altura)
+        for x in range(self.largura):
+            for y in range(self.altura):
+                valor_Ox = imagem_Ox.get_pixel(x, y)
+                valor_Oy = imagem_Oy.get_pixel(x, y)
+                valor_final = round(math.sqrt(valor_Ox ** 2 + valor_Oy ** 2))
+                # Garantir que o pixel resultante esteja no intervalo [0, 255]
+                valor_final = max(0, min(valor_final, 255))
+                imagem_final.set_pixel(x, y, valor_final)
+
+        return imagem_final
+
+    # Abaixo deste ponto estão utilitários para carregar, salvar e mostrar
+    # as imagens, bem como para a realização de testes. Você deve ler as funções
+    # abaixo para entendê-las e verificar como funcionam, mas você não deve
+    # alterar nada abaixo deste comentário.
+    #
+    # ATENÇÃO: NÃO ALTERE NADA A PARTIR DESTE PONTO!!! Você pode, no final
+    # deste arquivo, acrescentar códigos dentro da condicional
+    #
+    #                 if __name__ == '__main__'
+    #
+    # para executar testes e experiências enquanto você estiver executando o
+    # arquivo diretamente, mas que não serão executados quando este arquivo
+    # for importado pela suíte de teste e avaliação.
+    def __eq__(self, other):
+        return all(getattr(self, i) == getattr(other, i)
+                   for i in ('altura', 'largura', 'pixels'))
+
+    def __repr__(self):
+        return "Imagem(%s, %s, %s)" % (self.largura, self.altura, self.pixels)
+
+    @classmethod
+    def carregar(cls, nome_arquivo):
+        """
+        Carrega uma imagem do arquivo fornecido e retorna uma instância dessa
+        classe representando essa imagem. Também realiza a conversão para tons
+        de cinza.
+
+        Invocado como, por exemplo:
+           i = Imagem.carregar('test_images/cat.png')
+        """
+
+        with open(nome_arquivo, 'rb') as guia_para_imagem:
+            img = PILImage.open(guia_para_imagem)
+            img_data = img.getdata()
+            if img.mode.startswith('RGB'):
+                pixels = [round(.299 * p[0] + .587 * p[1] + .114 * p[2])
+                          for p in img_data]
+            elif img.mode == 'LA':
+                pixels = [p[0] for p in img_data]
+            elif img.mode == 'L':
+                pixels = list(img_data)
+            else:
+                raise ValueError('Modo de imagem não suportado: %r' % img.mode)
+            l, a = img.size
+            return cls(l, a, pixels)
+
+    @classmethod
+    def nova(cls, largura, altura):
+        """
+        Cria imagens em branco (tudo 0) com a altura e largura fornecidas.
+
+        Invocado como, por exemplo:
+            i = Imagem.nova(640, 480)
+        """
+        return cls(largura, altura, [0 for i in range(largura * altura)])
+
+    def salvar(self, nome_arquivo, modo='PNG'):
+        """
+        Salva a imagem fornecida no disco ou em um objeto semelhante a um arquivo.
+        Se o nome_arquivo for fornecido como uma string, o tipo de arquivo será
+        inferido a partir do nome fornecido. Se nome_arquivo for fornecido como
+        um objeto semelhante a um arquivo, o tipo de arquivo será determinado
+        pelo parâmetro 'modo'.
+        """
+        saida = PILImage.new(mode='L', size=(self.largura, self.altura))
+        saida.putdata(self.pixels)
+        if isinstance(nome_arquivo, str):
+            saida.save(nome_arquivo)
+        else:
+            saida.save(nome_arquivo, modo)
+        saida.close()
+
+    def gif_data(self):
+        """
+        Retorna uma string codificada em base 64, contendo a imagem
+        fornecida, como uma imagem GIF.
+
+        Função utilitária para tornar show_image um pouco mais limpo.
+        """
+        buffer = BytesIO()
+        self.salvar(buffer, modo='GIF')
+        return base64.b64encode(buffer.getvalue())
+
+    def mostrar(self):
+        """
+        Mostra uma imagem em uma nova janela Tk.
+        """
+        global WINDOWS_OPENED
+        if tk_root is None:
+            # Se Tk não foi inicializado corretamente, não faz mais nada.
+            return
+        WINDOWS_OPENED = True
+        toplevel = tkinter.Toplevel()
+        # O highlightthickness=0 é um hack para evitar que o redimensionamento da janela
+        # dispare outro evento de redimensionamento (causando um loop infinito de
+        # redimensionamento). Para maiores informações, ver:
+        # https://stackoverflow.com/questions/22838255/tkinter-canvas-resizing-automatically
+        tela = tkinter.Canvas(toplevel, height=self.altura,
+                              width=self.largura, highlightthickness=0)
+        tela.pack()
+        tela.img = tkinter.PhotoImage(data=self.gif_data())
+        tela.create_image(0, 0, image=tela.img, anchor=tkinter.NW)
+
+        def ao_redimensionar(event):
+            # Lida com o redimensionamento da imagem quando a tela é redimensionada.
+            # O procedimento é:
+            #  * converter para uma imagem PIL
+            #  * redimensionar aquela imagem
+            #  * obter os dados GIF codificados em base 64 (base64-encoded GIF data)
+            #    a partir da imagem redimensionada
+            #  * colocar isso em um label tkinter
+            #  * mostrar a imagem na tela
+            nova_imagem = PILImage.new(
+                mode='L', size=(self.largura, self.altura))
+            nova_imagem.putdata(self.pixels)
+            nova_imagem = nova_imagem.resize(
+                (event.width, event.height), PILImage.NEAREST)
+            buffer = BytesIO()
+            nova_imagem.save(buffer, 'GIF')
+            tela.img = tkinter.PhotoImage(
+                data=base64.b64encode(buffer.getvalue()))
+            tela.configure(height=event.height, width=event.width)
+            tela.create_image(0, 0, image=tela.img, anchor=tkinter.NW)
+
+        # Por fim, faz o bind da função para que ela seja chamada quando a tela
+        # for redimensionada:
+        tela.bind('<Configure>', ao_redimensionar)
+        toplevel.bind('<Configure>', lambda e: tela.configure(
+            height=e.height, width=e.width))
+
+        # Quando a tela é fechada, o programa deve parar
+        toplevel.protocol('WM_DELETE_WINDOW', tk_root.destroy)
+
+
+# Não altere o comentário abaixo:
+# noinspection PyBroadException
+try:
+    tk_root = tkinter.Tk()
+    tk_root.withdraw()
+    tcl = tkinter.Tcl()
+
+    def refaz_apos():
+        tcl.after(500, refaz_apos)
+
+    tcl.after(500, refaz_apos)
+except:
+    tk_root = None
+
+WINDOWS_OPENED = False
+
+if __name__ == '__main__':
+    # O código neste bloco só será executado quando você executar
+    # explicitamente seu script e não quando os testes estiverem
+    # sendo executados. Este é um bom lugar para gerar imagens, etc.
+
+    pass
+
+    # Questão 1 - Teste com o filtro de inversão da Imagem(4, 1, [29, 89, 136, 200])
+    # imagem 4 x 1
+    '''
+    imagem_teste = Imagem(4, 1, [29, 89, 136, 200])
+    imagem_invertida = imagem_teste.invertida()
+    print("Pixels da imagem invertida:", imagem_invertida.pixels)
+
+    # Comparação dos valores dos pixels com os valores esperados
+    esperado = Imagem(4, 1, [226, 166, 119, 55])
+    if imagem_invertida.pixels == esperado.pixels:
+        print("Os pixels da imagem invertida correspondem aos pixels esperados.")
+    else:
+        print("Os pixels da imagem invertida NÃO correspondem aos pixels esperados.")
+    '''
+
+    # Questão 2- Teste com o filtro de inversão da Imagem do Peixe. Deve salvar e mostrar a imagem invertida
+    # Exemplo de uso do filtro de inversão na imagem bluegill.png
+    '''
+    imagem_Peixe = Imagem.carregar('test_images/bluegill.png')
+    imagem_PeixeInvertida = imagem_Peixe.invertida()
+
+    # Salvar a imagem invertida como um arquivo PNG
+    imagem_PeixeInvertida.salvar('bluegill_invertida.png')
+
+    # Mostrar a imagem invertida
+    imagem_PeixeInvertida.mostrar()
+    '''
+
+    # Questão 3- Teste da imagem com kernel
+    # Crie uma instância da classe Imagem com os valores da imagem de entrada
+    '''
+    imagem_entrada = Imagem(3, 3, [80, 53, 99, 129, 127, 148, 175, 174, 193])
+
+    # Defina o kernel
+    kernel = [
+        [0.00, -0.07, 0.00],
+        [-0.45, 1.20, -0.25],
+        [0.00, -0.12, 0.00]
+    ]
+
+    # Chame a função correlacao passando o kernel como argumento
+    imagem_resultante = imagem_entrada.correlacao(kernel)
+
+    valor_pixel = imagem_resultante.get_pixel(1, 1)
+
+    print("Valor do pixel na posição (1, 1):", valor_pixel)
+    '''
+
+    # Questão 4- Imagem do Porco
+    '''
+    imagem_Porco = Imagem.carregar('test_images/pigbird.png')
+
+    # Definir o kernel 9x9
+    kernel = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    # Aplicar a correlação com o kernel na imagem
+    imagem_PorcoCorrelacao = imagem_Porco.correlacao(kernel)
+
+    # Salvar a imagem resultante
+    imagem_PorcoCorrelacao.salvar('pigbird_correlacao.png')
+
+    imagem_PorcoCorrelacao.mostrar()
+    
+
+    # Questão Gato borrado
+    imagem_gato = Imagem.carregar('test_images/cat.png')
+    imagem_Gatoborrada = imagem_gato.borrada(5)
+    imagem_Gatoborrada.salvar('cat_borrado.png')
+    imagem_Gatoborrada.mostrar()
+    '''
+
+    # Questão 5- Python com nitidez
+    '''
+    imagem_python = Imagem.carregar('test_images/python.png')
+    imagem_Pythonnitida = imagem_python.focada(11)
+    imagem_Pythonnitida.salvar('python_nitida.png')
+    imagem_Pythonnitida.mostrar()
+    '''
+
+    # Questão 6- Contrução com bordas
+    '''
+    imagem_construcao = Imagem.carregar('test_images/construct.png')
+    imagem_Construcaobordas = imagem_construcao.bordas()
+    imagem_Construcaobordas.salvar('construct_bordas.png')
+    imagem_Construcaobordas.mostrar()
+    '''
+    # O código a seguir fará com que as janelas de Imagem.mostrar
+    # sejam exibidas corretamente, quer estejamos executando
+    # interativamente ou não:
+    if WINDOWS_OPENED and not sys.flags.interactive:
+        tk_root.mainloop()
---
+++ pset1/test.py
@@ -0,0 +1,184 @@
+# IDENTIFICAÇÃO DO ESTUDANTE:
+# Preencha seus dados e leia a declaração de honestidade abaixo. NÃO APAGUE
+# nenhuma linha deste comentário de seu código!
+#
+#    Nome completo: João Pedro Estevão Louback
+#    Matrícula: 202305625
+#    Turma: CC3MA
+#    Email: jpestevaoloubackbr@gmail.com
+#
+# DECLARAÇÃO DE HONESTIDADE ACADÊMICA:
+# Eu afirmo que o código abaixo foi de minha autoria. Também afirmo que não
+# pratiquei nenhuma forma de "cola" ou "plágio" na elaboração do programa,
+# e que não violei nenhuma das normas de integridade acadêmica da disciplina.
+# Estou ciente de que todo código enviado será verificado automaticamente
+# contra plágio e que caso eu tenha praticado qualquer atividade proibida
+# conforme as normas da disciplina, estou sujeito à penalidades conforme
+# definidas pelo professor da disciplina e/ou instituição.
+#
+
+
+# Imports
+import os
+import pset1
+import unittest
+
+# Diretório
+TEST_DIRECTORY = os.path.dirname(__file__)
+
+
+# Classe para os testes de imagem:
+class TestImagem(unittest.TestCase):
+    def test_carregar(self):
+        resultado = pset1.Imagem.carregar('test_images/centered_pixel.png')
+        esperado = pset1.Imagem(11, 11,
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(resultado, esperado)
+
+
+# Classe para os testes de inversão:
+class TestInvertida(unittest.TestCase):
+    def test_invertida_1(self):
+        im = pset1.Imagem.carregar('test_images/centered_pixel.png')
+        resultado = im.invertida()
+        esperado = pset1.Imagem(11, 11,
+                                [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 0, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255])
+        self.assertEqual(resultado,  esperado)
+
+    def test_invertida_2(self):
+        # Questão 1 - Teste com o filtro de inversão da Imagem(4, 1, [29, 89, 136, 200])
+        # imagem 4 x 1
+        imagem = pset1.Imagem(4, 1, [29, 89, 136, 200])
+        resultado = imagem.invertida()
+        esperado = pset1.Imagem(4, 1, [226, 166, 119, 55])
+        self.assertTrue(resultado, esperado)
+
+    # teste para a imagem invertida do peixe.
+    def test_invertida_bluegill(self):
+        # Carregar a imagem bluegill.png
+        imagem_original = pset1.Imagem.carregar('test_images/bluegill.png')
+
+        # Aplicar o filtro de inversão
+        resultado = imagem_original.invertida()
+
+        # Criar a imagem esperada invertendo manualmente os pixels
+        esperado = pset1.Imagem(imagem_original.largura, imagem_original.altura,
+                                [255 - pixel for pixel in imagem_original.pixels])
+
+        # Verificar se a imagem invertida está correta
+        self.assertEqual(resultado.pixels, esperado.pixels)
+
+    # Teste para a função de correlação
+    def test_correlacao(self):
+        # Criar a imagem de entrada com os valores fornecidos
+        largura = 3
+        altura = 3
+        pixels = [80, 53, 99, 129, 127, 148, 175, 174, 193]
+        img = pset1.Imagem(largura, altura, pixels)
+
+        # Definir o kernel
+        kn = [
+            [0.00, -0.07, 0.00],
+            [-0.45, 1.20, -0.25],
+            [0.00, -0.12, 0.00]
+        ]
+
+        # Calcular o resultado da correlação
+        resultado_correlacao = img.correlacao(kn)
+
+        # Valor esperado
+        valor_esperado = 32.76  # Valor esperado calculado manualmente
+
+        # Verificar se o valor do pixel destacado está próximo do valor esperado
+        self.assertAlmostEqual(resultado_correlacao.get_pixel(
+            1, 1), valor_esperado, delta=0.01)
+
+    # Teste para a imagem do porco
+
+    def test_imagens_invertidas(self):
+        for nome_arquivo in ('mushroom', 'twocats', 'chess'):
+            with self.subTest(f=nome_arquivo):
+                arquivo_entrada = os.path.join(
+                    TEST_DIRECTORY, 'test_images', '%s.png' % nome_arquivo)
+                arquivo_saida = os.path.join(
+                    TEST_DIRECTORY, 'test_results', '%s_invert.png' % nome_arquivo)
+                resultado = pset1.Imagem.carregar(arquivo_entrada).invertida()
+                esperado = pset1.Imagem.carregar(arquivo_saida)
+                self.assertEqual(resultado,  esperado)
+
+
+# Classe para os testes dos filtros:
+class TestFiltros(unittest.TestCase):
+    def test_borrada(self):
+        for tamanho_kernel in (1, 3, 7):
+            for nome_arquivo in ('mushroom', 'twocats', 'chess'):
+                with self.subTest(k=tamanho_kernel, f=nome_arquivo):
+                    arquivo_entrada = os.path.join(
+                        TEST_DIRECTORY, 'test_images', '%s.png' % nome_arquivo)
+                    arquivo_saida = os.path.join(TEST_DIRECTORY, 'test_results',
+                                                 '%s_blur_%02d.png' % (nome_arquivo, tamanho_kernel))
+                    imagem_entrada = pset1.Imagem.carregar(arquivo_entrada)
+                    imagem_entrada_copia = pset1.Imagem(imagem_entrada.largura, imagem_entrada.altura,
+                                                        imagem_entrada.pixels)
+                    resultado = imagem_entrada.borrada(tamanho_kernel)
+                    esperado = pset1.Imagem.carregar(arquivo_saida)
+                    self.assertEqual(imagem_entrada, imagem_entrada_copia,
+                                     "Cuidado para não modificar a imagem original!")
+                    self.assertEqual(resultado,  esperado)
+
+    def test_focada(self):
+        for tamanho_kernel in (1, 3, 9):
+            for nome_arquivo in ('mushroom', 'twocats', 'chess'):
+                with self.subTest(k=tamanho_kernel, f=nome_arquivo):
+                    arquivo_entrada = os.path.join(
+                        TEST_DIRECTORY, 'test_images', '%s.png' % nome_arquivo)
+                    arquivo_saida = os.path.join(TEST_DIRECTORY, 'test_results',
+                                                 '%s_sharp_%02d.png' % (nome_arquivo, tamanho_kernel))
+                    imagem_entrada = pset1.Imagem.carregar(arquivo_entrada)
+                    imagem_entrada_copia = pset1.Imagem(imagem_entrada.largura, imagem_entrada.altura,
+                                                        imagem_entrada.pixels)
+                    resultado = imagem_entrada.focada(tamanho_kernel)
+                    esperado = pset1.Imagem.carregar(arquivo_saida)
+                    self.assertEqual(imagem_entrada, imagem_entrada_copia,
+                                     "Cuidado para não modificar a imagem original!")
+                    self.assertEqual(resultado,  esperado)
+
+    def test_bordas(self):
+        for nome_arquivo in ('mushroom', 'twocats', 'chess'):
+            with self.subTest(f=nome_arquivo):
+                arquivo_entrada = os.path.join(
+                    TEST_DIRECTORY, 'test_images', '%s.png' % nome_arquivo)
+                arquivo_saida = os.path.join(
+                    TEST_DIRECTORY, 'test_results', '%s_edges.png' % nome_arquivo)
+                imagem_entrada = pset1.Imagem.carregar(arquivo_entrada)
+                imagem_entrada_copia = pset1.Imagem(imagem_entrada.largura, imagem_entrada.altura,
+                                                    imagem_entrada.pixels)
+                resultado = imagem_entrada.bordas()
+                esperado = pset1.Imagem.carregar(arquivo_saida)
+                self.assertEqual(imagem_entrada, imagem_entrada_copia,
+                                 "Cuidado para não modificar a imagem original!")
+                self.assertEqual(resultado,  esperado)
+
+
+if __name__ == '__main__':
+    res = unittest.main(verbosity=3, exit=False)
